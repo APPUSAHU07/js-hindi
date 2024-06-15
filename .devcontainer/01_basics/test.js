ed282da5f9f315b25e1f53294c7ac0f31dc5401e
Appu_a_sahu <74091807+APPUSAHU07@users.noreply.github.com>
2024-06-15 07:20:02 +0000

    Intro

---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,16 @@
+console.log("HELLO WORLD");
+
+//DATATYPE CONVERSION
+let score = 33
+console.log(typeof score)
+
+let score_1 = "33"
+console.log(typeof score_1)
+
+let valueInNumber = Number(score_1)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+let score1 = Number(true)
+console.log(typeof score1);
+console.log(score1);
